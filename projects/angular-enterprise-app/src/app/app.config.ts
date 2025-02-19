f9a5340483afa3c07e1694f376caffd694412167
amin atwi <aminatwi@outlook.com>
2025-02-19 21:20:42 +0200

    add-eslint-file-with-boundaries-rules-and-resolve-unused-imports

--- projects/angular-enterprise-app/src/app/app.config.ts
+++ projects/angular-enterprise-app/src/app/app.config.ts
@@ -1,5 +1,4 @@
-import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
-import { provideRouter } from '@angular/router';
+import { ApplicationConfig } from '@angular/core';
 import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
 import { routes } from './app.routes';
 import { provideCore } from './core/core';
--- projects/angular-enterprise-app/src/app/core/core.ts
+++ projects/angular-enterprise-app/src/app/core/core.ts
@@ -6,7 +6,7 @@ import { provideStore } from '@ngrx/store';
 import { provideEffects } from '@ngrx/effects';
 import { provideStoreDevtools } from '@ngrx/store-devtools';
 import { TranslateHttpLoader } from '@ngx-translate/http-loader';
-import { HttpClient, HttpClientModule, provideHttpClient, withInterceptors } from "@angular/common/http";
+import { HttpClient, HttpClientModule} from "@angular/common/http";
 import { TranslateLoader, TranslateModule } from "@ngx-translate/core";
 import { environment } from "../../environments/environment";
 import { CoreEffects } from "./store/core.effects";
